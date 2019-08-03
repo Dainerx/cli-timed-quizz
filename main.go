ceba01873f92473e2af185b3895498de5b467da4
Dainerx <oussema.benghorbel@esprit.tn>
2019-08-03 14:48:44 +0200

    adding timer, go routines and channel to handle the timed quiz

--- main.go
+++ main.go
@@ -7,13 +7,31 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"time"
 )
 
+const QUIZ_DURATION = 60
+
 func main() {
+	fmt.Println("Welcome to CLI Timed Quiz!")
+	fmt.Println("You have ", QUIZ_DURATION, " seconds to finish the quiz")
+	timer := time.NewTimer(time.Second * QUIZ_DURATION)
 	tasks := readcsv("problems.csv")
 	answers := make(map[string]int)
 	for task := range tasks {
-		answers[task] = requestAnswer(task + ": ")
+		answerChannel := make(chan int)
+		go func() {
+			answers[task] = requestAnswer(task + ": ")
+			answerChannel <- answers[task]
+		}()
+		select {
+		case <-timer.C:
+			score, total := scoreAndTotal(tasks, answers)
+			fmt.Println("Your score is: ", score, "/", total)
+			return
+		case <-answerChannel:
+			continue
+		}
 	}
 	score, total := scoreAndTotal(tasks, answers)
 	fmt.Println("Your score is: ", score, "/", total)
@@ -22,7 +40,7 @@ func main() {
 func requestAnswer(message string) int {
 	var answer int
 	fmt.Print(message)
-	_, err := fmt.Scanf("%d", &answer)
+	_, err := fmt.Scanf("%d", &answer) //blocking call
 	if err != nil {
 		log.Fatal("Error reading")
 	}
@@ -33,6 +51,9 @@ func scoreAndTotal(correct, attempted map[string]int) (int, int) {
 	score := 0
 	total := len(correct)
 	for task := range correct {
+		if _, ok := attempted[task]; !ok {
+			continue
+		}
 		if correct[task] == attempted[task] {
 			score++
 		}
